530e96429717ad177418b7132f1d91954cded6cb
Sinta AT <sinta.tangkudung@dana.id>
2021-12-03 10:02:07 +0700

    Update peer direct message model

--- model/direct_message.go
+++ model/direct_message.go
@@ -10,15 +10,15 @@ type DirectMessage struct {
 	PeerID    int
 	MessageID int
 	Direction string
-	SeenAt    time.Time
+	SeenAt    *time.Time
 }
 
-type DirectMessageWithContent struct {
+type PeerDirectMessage struct {
 	ID             int
-	UserID         int
 	PeerID         int
 	MessageID      int
 	MessageContent string
 	Direction      string
-	SeenAt         time.Time
+	SeenAt         *time.Time
+	CreatedAt      time.Time
 }
