1b3cad8e1a6851cc17b0ba955ff0b2f40abcf884
Sinta AT <sinta.tangkudung@dana.id>
2021-12-03 10:53:47 +0700

    Update model

--- model/peer.go
+++ model/peer.go
@@ -1,8 +1,12 @@
 package model
 
+import (
+	"time"
+)
+
 type Peer struct {
-	ID                  string `json:"id"`
-	Username            string `json:"username"`
-	LatestMessageAt     int    `json:"latest_message_at"`
-	UnseenMessagesCount int    `json:"unseen_messages_count"`
+	ID                  string    `json:"id"`
+	Username            string    `json:"username"`
+	LatestMessageAt     time.Time `json:"latest_message_at"`
+	UnseenMessagesCount int       `json:"unseen_messages_count"`
 }
